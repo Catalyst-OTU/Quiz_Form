03e2e9f179ccc3876c05a29eb3902b4ff7562b75
Catalyst-OTU <bismarkotu1995@gmail.com>
2021-02-02 17:30:46 +0000

    first commit

---
+++ question.js
@@ -0,0 +1,85 @@
+
+var questions =[{
+    "question": "What does CPU stand for?",
+    "option1": "Computer Processing Unit",
+    "option2": "Central Peripheral Unit",
+    "option3": "Central Processing Unit",
+    "option4": "Computer Processing Unit",
+    "option5": "None of the above",
+    "answer": "3"
+ }, {
+    "question": "Who invented the Graphical User Interface (GUI)?",
+    "option1": "Microsoft",
+    "option2": "Apple",
+    "option3": "Dell",
+    "option4": "Xerox",
+    "option5": "HP",
+    "answer": "3"
+ }, {
+    "question": "What does CPU stand for?",
+    "option1": "Computer Processing Unit",
+    "option2": "Central Peripheral Unit",
+    "option3": "Central Processing Unit",
+    "option4": "Computer Processing Unit",
+    "option5": "None of the above",
+    "answer": "3"
+ }, {
+    "question": "What does CPU stand for?",
+    "option1": "Computer Processing Unit",
+    "option2": "Central Peripheral Unit",
+    "option3": "Central Processing Unit",
+    "option4": "Computer Processing Unit",
+    "option5": "None of the above",
+    "answer": "3"
+ }, {
+    "question": "Who invented the Graphical User Interface (GUI)?",
+    "option1": "Microsoft",
+    "option2": "Apple",
+    "option3": "Dell",
+    "option4": "Xerox",
+    "option5": "HP",
+    "answer": "3"
+ }, {
+    "question": "What does CPU stand for?",
+    "option1": "Computer Processing Unit",
+    "option2": "Central Peripheral Unit",
+    "option3": "Central Processing Unit",
+    "option4": "Computer Processing Unit",
+    "option5": "None of the above",
+    "answer": "3"
+ }, {
+    "question": "What does CPU stand for?",
+    "option1": "Computer Processing Unit",
+    "option2": "Central Peripheral Unit",
+    "option3": "Central Processing Unit",
+    "option4": "Computer Processing Unit",
+    "option5": "None of the above",
+    "answer": "3"
+ }, {
+    "question": "Who invented the Graphical User Interface (GUI)?",
+    "option1": "Microsoft",
+    "option2": "Apple",
+    "option3": "Dell",
+    "option4": "Xerox",
+    "option5": "HP",
+    "answer": "3"
+ }, {
+    "question": "What does CPU stand for?",
+    "option1": "Computer Processing Unit",
+    "option2": "Central Peripheral Unit",
+    "option3": "Central Processing Unit",
+    "option4": "Computer Processing Unit",
+    "option5": "None of the above",
+    "answer": "3"
+ }, {
+    "question": "Who invented the Graphical User Interface (GUI)?",
+    "option1": "Microsoft",
+    "option2": "Apple",
+    "option3": "Dell",
+    "option4": "Xerox",
+    "option5": "HP",
+    "answer": "3"
+ }, 
+
+
+]
